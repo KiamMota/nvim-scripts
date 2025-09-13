81648a6d1264e23cd08e8b3dff5dcb47adaa524c
KiamMota <somentekiam@gmail.com>
2025-09-13 13:02:13 -0300

    add : wrong word

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,12 +1,17 @@
 local corrections = {
-  ["include"] = {"INcluide", "incldue", "inclide", "Include"},
+  ["include"] = {"INcluide", "incldue", "inclide", "Include", "incblduye", "incluyde"},
   ["length"] = {"lenght", "lengh"},
   ["namespace"] = {"namesapce", "namespce"},
   ["return"] = {"retun", "retrn"},
   ["width"] = {"witdht", "widht"},
   ["function"] = {"fucntion", "functoin"},
   ["const"] = {"constr", "cosnt"},
-  ["class"] = {"calss", "lcass"}
+  ["class"] = {"calss", "lcass", "clas"},
+  ["void"] = {"voud", "vodi"},
+  ["define"] = {"DEFINE", "deifnde", "deinfe", "dei0fne"},
+  ["while"] = {"wilhe", "wihle", "whie"},
+  ["switch"] = {"siwtch", "swithc", "esicjh", "sich", "swtich,", "stich"},
+  
 }
 
 for correct, wrongs in pairs(corrections) do
